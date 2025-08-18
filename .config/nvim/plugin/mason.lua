f098f39d12f46ae31223e10e3f64ca0f3ced9628
rolo <rolo@MacBook-Pro-de-rolo.local>
2025-08-17 21:21:58 -0400

    chore: fix mason servers

--- .config/nvim/plugin/mason.lua
+++ .config/nvim/plugin/mason.lua
@@ -5,10 +5,15 @@ local status2, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then return end
 
 mason.setup({
-  PATH = "skip"  -- o "prepend" si quieres
+  --PATH = "skip"  -- o "prepend" si quieres
 })
 
 mason_lspconfig.setup({
   automatic_installation = true,
-  ensure_installed = { "tsserver", "html", "cssls", "jsonls" }
+  ensure_installed = {
+    "tsserver",
+    "html",
+    "cssls",
+    "lua_ls"
+  }
 })
