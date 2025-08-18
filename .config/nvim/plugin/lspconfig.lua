0b4b4a9075b23c3a6f71cfc253c6abc191cb9785
rholo <rolo@MacBook-Pro-de-rolo.local>
2025-08-17 21:38:47 -0400

    chore: fix mason servers

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -1,6 +1,25 @@
 local status, lsp_config = pcall(require, 'lspconfig')
 if (not status) then return end
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local on_attach = function(_none, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  --local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  --buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap = true, silent = true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  -- buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
+
 lsp_config.ts_ls.setup {
   on_attach = on_attach,
   filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx', 'javascript' },
